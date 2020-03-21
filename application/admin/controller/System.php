8a6e38d592d8997832d29e868d2eda9cff5e6e4f
贺命雄 <705514898@qq.com>
2020-03-21 11:10:09 +0800

    hmx页面模块化
    Signed-off-by: 贺命雄 <705514898@qq.com>

--- application/admin/controller/System.php
+++ application/admin/controller/System.php
@@ -14,7 +14,7 @@ class System extends Controller
 {
     public function initialize(){
         if(!session('admin_id')){
-            $this->redirect("http://www.jsh.com/view/login");
+            $this->redirect("/view/login");
         }
     }
 }
